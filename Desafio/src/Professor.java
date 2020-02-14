846c51c88f9ffd523ec4147836c1b2684a3ef64a
4ZUK <60348334+4zuk@users.noreply.github.com>
2020-02-14 11:23:47 -0300

    Parte C ok

---
+++ Desafio/src/Professor.java
@@ -0,0 +1,42 @@
+public class Professor {
+    public String nomeProf;
+    public String sobrenomeProf;
+    public Integer tempoDeCasa;
+    public Integer codProf;
+
+    public String getNomeProf() {
+        return nomeProf;
+    }
+
+    public void setNomeProf(String nomeProf) {
+        this.nomeProf = nomeProf;
+    }
+
+    public String getSobrenomeProf() {
+        return sobrenomeProf;
+    }
+
+    public void setSobrenomeProf(String sobrenomeProf) {
+        this.sobrenomeProf = sobrenomeProf;
+    }
+
+    public Integer getTempoDeCasa() {
+        return tempoDeCasa;
+    }
+
+    public void setTempoDeCasa(Integer tempoDeCasa) {
+        this.tempoDeCasa = tempoDeCasa;
+    }
+
+    public Integer getCodProf() {
+        return codProf;
+    }
+
+    public void setCodProf(Integer codProf) {
+        this.codProf = codProf;
+    }
+
+    public Professor() {
+        super();
+    }
+}
